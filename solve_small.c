4efa697d1d8d292c9abb4d5b3452e58ca81cb6b9
Alexandre DE TEMMERMAN <ade-temm@e1r11p8.42.fr>
2021-10-07 10:13:22 +0200

    small solve works

--- solve_small.c
+++ solve_small.c
@@ -6,7 +6,7 @@
 /*   By: ade-temm <ade-temm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/04 14:48:22 by ade-temm          #+#    #+#             */
-/*   Updated: 2021/10/06 17:05:02 by ade-temm         ###   ########.fr       */
+/*   Updated: 2021/10/07 10:11:22 by ade-temm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,27 +19,31 @@ int		check_values(int a, int b)
 
 void	solve_small_stack(int *a, int *size)
 {
-	int		*size_a;
 	int		i;
 
-	size_a = size;
 	i = 0;
-	while(i < (*size) - 1)
+	if(*size > 1)
 	{
-		if(check_values(a[i], a[i + 1]) < 0)
+		while(check_if_solved(a, *size) != 0)
 		{
-			if(i == 0)
+			i = 0;
+			while(i < 2)
 			{
-				printf("SA\n");
-				sa(a);
-			}
-			else
-			{
-				printf("RRA\n");
-				rra(&a, *size_a);
+				if(a[i] > a[i + 1])
+				{
+					if(i == 0)
+					{
+						printf("SA\n");
+						sa(a);
+					}
+					else{
+						printf("RRA\n");
+						rra(&a, *size);
+					}
+				}
+				i++;
 			}
 		}
-		i++;
 	}
 	return ;
 }
